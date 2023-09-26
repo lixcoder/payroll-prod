e0fbb775077d02ecf572be54604efac515e05041
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2023-09-26 17:19:58 +0300

    Update License.php

--- app/Models/License.php
+++ app/Models/License.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class License extends Model
 {
     //
-    protected $table = 'licenses';
+    protected $table = 'subscriptions';
+
+    public static function checkSubscription($id){
+        $currentSub = License::where('business_id', $id)->get();
+
+        if(count($currentSub)>=1){
+           $currentSub = json_decode($currentSub, true); 
+
+            if(Carbon::now()->greaterThan(Carbon::parse($currentSub[0]['trial_end_date']))){
+                //indicates new subscrition needed to keep using services
+               return 0;
+            }else{
+                //indicates subscrition is still active
+                return 1;
+            }
+            
+        }
+        else{
+            //indicates new subscrition needed to keep using services
+            return 0;
+        }
+    }
 }
