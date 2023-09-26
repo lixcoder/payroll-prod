bbdcc20c0f2a44a272f3284c7818e26dd7fdb399
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2023-09-26 12:17:00 +0300

    Update Subscriptions.php

--- app/Models/Subscriptions.php
+++ app/Models/Subscriptions.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Subscriptions extends Model
 {
     
-    
+    public $table = 'subscriptions';
+
+    public static function checkSubscription($id){
+        $currentSub = Sbscription::where('business_id', $id)->get();
+        return json_decode($currentSub);
+    }
 }
