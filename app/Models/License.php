5cf506c113a64f65a9571b96653e81ae9da05457
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2023-09-29 10:46:24 +0300

    Update License.php

--- app/Models/License.php
+++ app/Models/License.php
@@ -19,10 +19,10 @@ class License extends Model
             if(Carbon::now()->greaterThan(Carbon::parse($currentSub[0]['trial_end_date']))  || $currentSub[0]['status'] == 'aproved' || $currentSub[0]['deleted_at'] == NULL){
                 // && $currentSub[0]['status'] == 'approved' && $currentSub[0]['deleted_at'] == NULL
                 //indicates new subscrition needed to keep using services
-               return 0;
+               return var_dump($currentSub[0]['deleted_at']);
             }else{
                 //indicates subscrition is still active
-                return var_dump($currentSub[0]['deleted_at']);
+                return 1;
             }
             
         }
