b6fc7fd87d38b1d388c7db99de41581ddf9e6c3e
Titus Kimutai <tkimutai276@gmail.com>
2023-10-03 12:19:22 +0300

    nssf

--- resources/views/nssf/index.blade.php
+++ resources/views/nssf/index.blade.php
@@ -8,96 +8,77 @@
                 <div class="page-body">
                     <div class="row">
                         <div class="col-lg-12">
-                            <h3>New Nssf Rate</h3>
+                            <h3>Nssf Rates</h3>
 
                             <hr>
                         </div>
                         <div class="col-lg-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    @if ($errors)
-                                        @foreach ($errors->all() as $error)
-                                            <div class="alert alert-danger">
-                                                {{ $error }}<br>
-                                            </div>
+                            <div class="md-card-content" style="overflow-x: auto;">
+                                <div class="card-body" style="width: 500px">
+                                    <div class="mb-2">
+                                        <a class="btn btn-info btn-sm" href="{{ URL::to('nssf/create')}}">new nssf
+                                            rate</a>
+                                    </div>
+                                    <table id="users"
+                                           class="table table-condensed table-bordered table-hover">
+                                        <thead>
+                                        <tr>
+                                            
+                                            <th>id</th>
+                                            <th>employee contribution</th>
+                                            <th>employer contribution</th>
+                                            <th>total contribution</th>
+                                            <th>maximum employee nssf</th>
+                                            <th>maximum employer nssf</th>
+                                            <th>nssf lower earning</th>
+                                            <th>nssf upper earning</th>
+                                            <th>employer nssf upper earning</th>
+                                            <th>graduated scale</th>
+                                            <th>Action</th>
+                                            
+                                            
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php $i = 1; ?>
+                                        @foreach($nrates as $nrate)
+                                            <tr>
+                                                <td>
+
+                                                <div class="btn-group">
+                                                    <button type="button"
+                                                            class="btn btn-info btn-sm dropdown-toggle"
+                                                            data-toggle="dropdown" aria-expanded="false">
+                                                        Action <span class="caret"></span>
+                                                    </button>
+
+                                                    <ul class="dropdown-menu" role="menu">
+                                                        <li>
+                                                            <a href="{{URL::to('nssf/edit/'.$nrate->id)}}">Update</a>
+                                                        </li>
+
+                                                        <li>
+                                                            <a href="{{URL::to('nssf/delete/'.$nrate->id)}}">Delete</a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                                </td>
+                                                <td> {{ $i }}</td>
+                                                <td>{{ $nrate-> employee_contribution}}</td>
+                                                <td>{{ $nrate->employer_contribution }}</td>
+                                                <td>{{ $nrate->total_contribution }}</td>
+                                                <td>{{ $nrate->max_employee_nssf }}</td>
+                                                <td>{{ $nrate->max_employer_nssf }}</td>
+                                                <td>{{ $nrate->nssf_lower_earning }}</td>
+                                                <td>{{ $nrate->nssf_upper_earning }}</td>
+                                                <td>{{ $nrate->employer_nssf_upper_earning }}</td>
+                                                <td>{{ $nrate->graduated_scale }}</td>
+                                
+                                            </tr>
+                                            <?php $i++; ?>
                                         @endforeach
-                                    @endif
-                                    <form method="POST" action="{{{ URL::to('nssf') }}}" accept-charset="UTF-8">
-                                        @csrf
-                                        <fieldset>
-                                            <div class="form-group">
-                                                <label for="employee_contribution">employee contribution <span style="color:red">*</span> </label>
-                                                <input class="form-control" placeholder="" type="text" name="employee_contribution"
-                                                       id="employee_contribution"
-                                                       value="{{{ old('employee_contribution') }}}">
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="employer_contribution">employer contribution <span style="color:red">*</span>
-                                                </label>
-                                                <input class="form-control" placeholder="" type="text" name="employer_contribution"
-                                                       id="employer_contribution"
-                                                       value="{{{ old('employer_contribution') }}}">
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="total_contribution">Total contribution <span style="color:red">*</span>
-                                                </label>
-                                                <input class="form-control" placeholder="" type="text" name="total_contribution"
-                                                       id="total_contribution"
-                                                       value="{{{ old('total_contribution') }}}">
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="max_employee_nssf">maximum employee nssf <span style="color:red">*</span>
-                                                </label>
-                                                <input class="form-control" placeholder="" type="text" name="max_employee_nssf"
-                                                       id="max_employee_nssf"
-                                                       value="{{{ old('max_employee_nssf') }}}">
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="max_employer_nssf">maximum employer nssf <span style="color:red">*</span>
-                                                </label>
-                                                <input class="form-control" placeholder="" type="text" name="max_employer_nssf"
-                                                       id="max_employer_nssf"
-                                                       value="{{{ old('max_employer_nssf') }}}">
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="nssf_lower_earning">nssf lower earning <span style="color:red">*</span>
-                                                </label>
-                                                <input class="form-control" placeholder="" type="text"
-                                                       name="nssf_lower_earning"
-                                                       id="nssf_lower_earning" value="{{{ old('nssf_lower_earning') }}}">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="nssf_upper_earning">nssf upper earning <span style="color:red">*</span>
-                                                </label> 
-                                                <input class="form-control" placeholder="" type="text"
-                                                       name="nssf_upper_earning"
-                                                       id="nssf_upper_earning" value="{{{ old('nssf_upper_earning') }}}">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="employer_nssf_upper_earning">employer nssf upper earning <span style="color:red">*</span>
-                                                </label>
-                                                <input class="form-control" placeholder="" type="text"
-                                                       name="employer_nssf_upper_earning"
-                                                       id="employer_nssf_upper_earning" value="{{{ old('employer_nssf_upper_earning') }}}">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="graduated_scale">graduated scale <span style="color:red">*</span>
-                                                </label>
-                                                <input class="form-control" placeholder="" type="text"
-                                                       name="graduated_scale"
-                                                       id="graduated_scale" value="{{{ old('graduated_scale') }}}">
-                                            </div>
-                                            <div class="form-actions form-group">
-                                                <button type="submit" class="btn btn-primary btn-sm">Create Nssf Rate
-                                                </button>
-                                            </div>
-                                        </fieldset>
-                                    </form>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
@@ -106,4 +87,21 @@
             </div>
         </div>
     </div>
-@endsection
+    <div class="row">
+    </div>
+
+
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+@stop
