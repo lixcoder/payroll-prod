b51993c6566d8bb96bfe7036241b529c9e361eeb
Nelson <nelsohnkiprono@gmail.com>
2023-10-04 10:17:12 +0000

    Committing local changes

--- resources/views/pdf/p9Pdf.blade.php
+++ resources/views/pdf/p9Pdf.blade.php
@@ -1,100 +1,145 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: center;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-
-    </style>
-<<<<<<< HEAD
-    
-
-=======
->>>>>>> 24d37a2e34bb5d6b5fe49b7b53e653142c5a7f96
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-B4gt1pOupzpwat3uBbFvB18foO0vcyK0upC3sLcDcYLQLiDZwMhqy4pzw2R4lJ1w" crossorigin="anonymous">
 </head>
+<style>
+        .table-container {
+            overflow-x: auto; 
+        }
+
+        @media print {
+            body {
+                font-size: 12px; 
+            }
+
+            .table th,
+            .table td {
+                padding: 5px;
+            }
+
+            .card {
+                page-break-before: always; 
+            }
+        }
+    </style>
 <body>
-<div class="card">
-<center>
-    <img src="https://www.kra.go.ke/templates/kra/images/kra/logo.png" alt="logo">
-    <P>kenya Revenue Authority</P>
-    <P>Domestic Taxes Department</P>
-    <P>Tax Deduction Card Year 2022</P>
-</center>
-<<<<<<< HEAD
-    
-<table class="table">
-    
-    <tr>
-        <td>
-            Employer Name: {{$organization}}
-        </td>
-        <td style="text-align: right">
-=======
-</div>
+        <center>
+            <img src="https://www.kra.go.ke/templates/kra/images/kra/logo.png" alt="logo" class="img-fluid">
+            <P>Kenya Revenue Authority</P>
+            <P>Domestic Taxes Department</P>
+            <P>Tax Deduction Card Year 2022</P>
+        </center>
 
+        <div class="row">
+            <div class="col-lg-6">
+                <table class="table table-bordered">
+                    <tr>
+                        <th>
+                            Employer Name: Company
+                        </th>
+                        <th style="text-align: right">
+                            Employer's Pin: KRA PIN
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>
+                            Employee's Main Name: Nelson Sammy
+                        </th>
+                    </tr>
+                    <tr >
+                        <th>
+                            Employee's Other Names: Ngei
+                        </th>
+                        <th style="text-align: right">
+                            Employee's Pin 1101001
+                        </th>
+                    </tr>
+                </table>
+            </div>
+        </div>
 
-<table class="table table-bordered table-stripped mt-3">
-    <tr>
-        <th>
-            Employer Name: Company
-        </th>
-        <th style="text-align: right">
->>>>>>> 24d37a2e34bb5d6b5fe49b7b53e653142c5a7f96
-            Employer's Pin: KRA PIN
-        </th>
-    </tr>
-    <tr>
-        <th>
-            Employee's Main Name: NELson Sammy
-        </th>
-    </tr>
-    <tr >
-        <th>
-            Employee's Other Names: Ngei
-        </th>
-        <th style="text-align: right">
-            Employee's Pin 1101001
-        </th>
-    </tr>
-</table>
-
-<div class="col-lg-12">
-  <div class="md-card" style="overflow-x: scroll;">
-    <div class="card-body" style="width: 100px">
-    <table id="data" 
-      class="table table-condensed table-bordered table-hover">
-        <tr>
-            <th>Month</th>
-            <th>Basic Salary</th>
-            <th>Benefits Non Cash</th>
-            <th>Value of Quarters</th>
-            <th>Total Gross Pay</th>
-            <th>Defined Contribution Retirement Scheme</th>
-            <th>Owner Occupied Interest</th>
-            <th>Retirement Contribution & Owner Occupied Interest</th>
-            <th>Chargeable Pay</th>
-            <th>Tax Charged</th>
-            <th>Personal Relief</th>
-            <th>PAYE Tax (J-K)</th>
-        </tr>
-    </table>
+    <div class="table-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="md-table-content" style="overflow-x: auto;">
+                    <div class="table-body" style="width: 500px">
+                        <table id="data" class="table table-condensed table-bordered table-hover">
+                            <tr>
+                                <th>Month</th>
+                                <th>
+                                    Basic Salary
+                                    <p>Ksh</p>
+                                </th>
+                                <th>
+                                    Benefits Non Cash
+                                    <p>Ksh</p>
+                                </th>
+                                <th>
+                                    Value of Quarters
+                                    <p>Ksh</p>
+                                </th>
+                                <th style="text-align: justify; width: 90px">
+                                    Total Gross Pay
+                                    <p>Ksh</p>
+                                </th>
+                                <th>
+                                    Defined Contribution
+                                    <br/>
+                                    Retirement Scheme
+                                    <p>Ksh</p>
+                                </th>
+                                <th>
+                                    Owner Occupied Interest
+                                    <p>Ksh</p>
+                                </th>
+                                <th>
+                                    Retirement
+                                    <br>
+                                    Contribution &
+                                    <br>
+                                    Owner Occupied
+                                    <br>
+                                    Interest
+                                    <p>Ksh</p>
+                                </th>
+                                <th>
+                                    Chargeable Pay
+                                    <p>Ksh.</p>
+                                </th>
+                                <th>
+                                    Tax Charged
+                                    <p>Ksh.</p>
+                                </th>
+                                <th>
+                                    Personal Relief
+                                    <p>Ksh.</p>
+                                </th>
+                                <th>
+                                    Insurance Relief
+                                    <p>Ksh.</p>
+                                </th>
+                                <th>
+                                    Paye Tax
+                                    <br>
+                                    (J-K)
+                                    <p>Ksh.</p>
+                                </th>
+                            </tr>
+                        </table>
+                     </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-    
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SWEEWgnZkS5L0r3yAAdVT5M0E7AQdX1TVOU82H/bR2Pr5rA/YauFIM1KI+T5lC6Z" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-OU6eV6U2nQI3nOU5lDMOifG5cL0stR8FgCsp5dD/ByW1xt2tUdm8v95t9zHo5lMG9" crossorigin="anonymous"></script>
 </body>
 </html>
