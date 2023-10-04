24d37a2e34bb5d6b5fe49b7b53e653142c5a7f96
Titus Kimutai <tkimutai276@gmail.com>
2023-10-04 11:06:26 +0300

    pdf

--- resources/views/pdf/p9Pdf.blade.php
+++ resources/views/pdf/p9Pdf.blade.php
@@ -1,28 +1,37 @@
-<html lang="">
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
     <style>
-        p {
-            text-transform: uppercase;
+        table {
+            border-collapse: collapse;
+            width: 100%;
         }
-
-        #data th {
+        th, td {
             border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+        }
+        th {
+            background-color: #f2f2f2;
         }
     </style>
-
 </head>
 <body>
+<div class="card">
 <center>
     <img src="https://www.kra.go.ke/templates/kra/images/kra/logo.png" alt="logo">
     <P>kenya Revenue Authority</P>
     <P>Domestic Taxes Department</P>
     <P>Tax Deduction Card Year 2022</P>
 </center>
-<table class="table">
+</div>
+
+
+<table class="table table-bordered table-stripped mt-3">
     <tr>
         <th>
             Employer Name: Company
@@ -45,69 +54,30 @@
         </th>
     </tr>
 </table>
-<table id="data" class="table table-bordered">
-    <tr>
-        <th>MONTH</th>
-        <th>
-            Basic Salary
-            <p>Ksh</p>
-        </th>
-        <th>
-            Benefits Non Cash
-            <p>Ksh</p>
-        </th>
-        <th>
-            Value of Quarters
-            <p>Ksh</p>
-        </th>
-        <th style="text-align: justify; width: 90px">
-            Total Gross Pay
-            <p>Ksh</p>
-        </th>
-        <th>
-            Defined Contribution
-            <br/>
-            Retirement Scheme
-            <p>Ksh</p>
-        </th>
-        <th>
-            Owner Occupied Interest
-            <p>Ksh</p>
-        </th>
-        <th>
-            Retirement
-            <br>
-            Contribution &
-            <br>
-            Owner Occupied
-            <br>
-            Interest
-            <p>Ksh</p>
-        </th>
-        <th>
-            Chargeable Pay
-            <p>Ksh.</p>
-        </th>
-        <th>
-            Tax Charged
-            <p>Ksh.</p>
-        </th>
-        <th>
-            Personal Relief
-            <p>Ksh.</p>
-        </th>
-        <th>
-            Insurance Relief
-            <p>Ksh.</p>
-        </th>
-        <th>
-            Paye Tax
-            <br>
-            (J-K)
-            <p>Ksh.</p>
-        </th>
-    </tr>
-</table>
+
+<div class="col-lg-12">
+  <div class="md-card" style="overflow-x: scroll;">
+    <div class="card-body" style="width: 100px">
+    <table id="data" 
+      class="table table-condensed table-bordered table-hover">
+        <tr>
+            <th>Month</th>
+            <th>Basic Salary</th>
+            <th>Benefits Non Cash</th>
+            <th>Value of Quarters</th>
+            <th>Total Gross Pay</th>
+            <th>Defined Contribution Retirement Scheme</th>
+            <th>Owner Occupied Interest</th>
+            <th>Retirement Contribution & Owner Occupied Interest</th>
+            <th>Chargeable Pay</th>
+            <th>Tax Charged</th>
+            <th>Personal Relief</th>
+            <th>PAYE Tax (J-K)</th>
+        </tr>
+    </table>
+    </div>
+  </div>
 </div>
+    
 </body>
 </html>
