f1d27699e3df728b53980f44c1fcb8b1dde7d3df
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2023-10-11 16:26:09 +0300

    Update NhifController.php

--- app/Http/Controllers/NhifController.php
+++ app/Http/Controllers/NhifController.php
@@ -27,37 +27,11 @@ class NhifController extends BaseController {
     public function recieveJson(){
         header("Content-Type: application/json");
         $res = file_get_contents('php://input');
-//         $res = '{
-//    "Body": {
-//       "stkCallback": {
-//          "MerchantRequestID": "29115-34620561-1",
-//          "CheckoutRequestID": "ws_CO_191220191020363925",
-//          "ResultCode": 0,
-//          "ResultDesc": "The service request is processed successfully",
-//          "CallbackMetadata": {
-//             "Item": [
-//                {
-//                   "Name": "Amount",
-//                   "Value": "1.00"
-//                },
-//                {
-//                   "Name": "MpesaReceiptNumber",
-//                   "Value": "NLJ7RT61SV"
-//                },
-//                {
-//                   "Name": "TransactionDate",
-//                   "Value": 20191219102115
-//                },
-//                {
-//                   "Name": "PhoneNumber",
-//                   "Value": "254708374149"
-//                }
-//             ]
-//          }
-//       }
-//    }
-// }';
         $data = json_decode($res, true);
+
+        // Log the $data variable to a log file
+        Log::info('Logging $data variable:', ['data' => $data]);
+
         
         // Access the elements
         $MerchantRequestID = $data['Body']['stkCallback']['MerchantRequestID'];
