21df28210ffca1687cb7fddecb4878060965c172
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2023-10-16 11:48:34 +0300

    Update NhifController.php

--- app/Http/Controllers/NhifController.php
+++ app/Http/Controllers/NhifController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
 
 class NhifController extends BaseController {
 
