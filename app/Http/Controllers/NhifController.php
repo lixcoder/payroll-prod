c9da2219cfa15c1feb098e357ebf570756e171f6
Sir-Dommy <108345471+Sir-Dommy@users.noreply.github.com>
2023-11-22 13:28:12 +0300

    Update NhifController.php

--- app/Http/Controllers/NhifController.php
+++ app/Http/Controllers/NhifController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\NhifRates;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\View;
